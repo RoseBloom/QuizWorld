10c06766f17b5ae1191956d7836d2431d4835130
Steve <Steve.mccann@live.com>
2018-03-06 09:56:20 -0500

    Commit

---
+++ QuizBackend/QuizBackend/Controllers/QuestionsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizBackend.Models;
+
+namespace QuizBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Questions")]
+    public class QuestionsController : Controller
+    {
+
+        // POST api/Questions
+        [HttpPost]
+        public void Post([FromBody]Question question)
+        {
+        }
+    }
+}
---
+++ QuizBackend/QuizBackend/Models/Question.cs
@@ -0,0 +1,7 @@
+﻿namespace QuizBackend.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+    }
+}
